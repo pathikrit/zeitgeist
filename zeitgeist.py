991e57453ffec9726f66523c289b0272e0714a5b
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2025-11-14 17:01:33 -0500

    fix typo

--- zeitgeist.py
+++ zeitgeist.py
@@ -236,7 +236,7 @@ async def _(Agent, BaseModel, EVENTS_MODEL, Field, RETRIES, about_me):
             "<task>"
             "List key macro events and catalysts that might impact me"
             "e.g. FED or FOMC meetings, rate cuts, govt shutdowns, major economic prints, major earnings calls, "
-            "investor days, 13F, regulatory events, diplomatic events & visits, trade talks, supreme court descisions
+            "investor days, 13F, regulatory events, diplomatic events & visits, trade talks, supreme court descisions"
             "tech days, demos, analyst events, headline numbers, trade talks etc"
             "Avoid generic events or ongoing events (like wars) without any concrete timelines or concrete resolutions"
             "Also avoid things that can happen far in the future (>1 yr from now)"
